a76841798fcd0cbee49f370a7132ce40830956ed
Michael <c21502889@mytudubllin.ie>
2024-04-05 19:37:25 +0100

    Started code for triangle strip mesh, basic grid is now displayed

--- java/src/c21502889/MichaelsVisual.java
+++ java/src/c21502889/MichaelsVisual.java
@@ -3,44 +3,62 @@ package c21502889;
 import processing.core.*;
 import ie.tudublin.Visual;
 
-public class MichaelsVisual extends Visual 
-{
-  public void settings() 
-  {
-    size(1024, 500);
-    
+public class MichaelsVisual extends Visual {
+  // Class variables
+  int width = 600;  //Width of triangle mesh
+  int height = 600; //Height of triangle mesh
+  int scale = 20; //scale for the mesh
+
+  // Mesh rows and columns
+  int cols = width / scale;
+  int rows = height / scale;
+
+  
+  public void settings() {
+    size(600, 600, P3D);
 
     // Use this to make fullscreen
     // fullScreen();
 
     // Use this to make fullscreen and use P3D for 3D graphics
     // fullScreen(P3D, SPAN);
-  
-  }//End settings
 
-  public void setup() 
-  {
+  }// End settings
+
+  public void setup() {
     startMinim();
     background(0);
+    stroke(100, 149, 237); // Set the stroke colour to blue
+    noFill();
 
     // Call loadAudio to load an audio file to process
     loadAudio("EverythingInItsRightPlace.mp3");
 
-  }//End setup
+  }// End setup
 
-  public void keyPressed() 
-  {
-    if (key == ' ') 
-    {
+  public void keyPressed() {
+    if (key == ' ') {
       getAudioPlayer().cue(0);
       getAudioPlayer().play();
     }
-  
-  }//End keyPressed
 
-  public void draw ()
-  {
+  }// End keyPressed
 
+  public void draw() {
+
+    // Draw mesh with triangle strips
+    for (int y = 0; y < rows; y++)
+    {
+      beginShape(TRIANGLE_STRIP);
+
+      for (int x = 0; x < cols; x++)
+      {
+        vertex (x * scale, y * scale);
+        vertex(x * scale, (y + 1) * scale);
+      }
+      endShape();
+    }
+   
   }
 
 }// End MichaelsVisual
